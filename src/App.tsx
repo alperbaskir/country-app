438973cd77e7985891ec5facfa6cf6cdd20a8833
alperbaskir <alper.baskir@gmail.com>
2022-04-04 20:13:24 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "components/Search";
+function App() {
+  return (
+   <React.Fragment>
+     <Search/>
+     <div>I'm going to add selected country component here!</div>
+   </React.Fragment> 
+  
+  );
+}
+
+export default App;
---
+++ src/components/Search.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Search =  () => {
+    return (
+        <div>I'm going to add search box here</div>
+        );
+}
+
+export default Search;
